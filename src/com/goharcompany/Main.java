7ad491d6be0f1c77c6477bc1ce06807ad8fc4b4b
goharSharf <gohar.sharf@thinkbiganalytics.com>
2018-04-30 10:43:45 +0500

    Second Commit

--- src/com/goharcompany/Main.java
+++ src/com/goharcompany/Main.java
@@ -3,6 +3,7 @@ package com.goharcompany;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code
+        System.out.println("Hello");
     }
 }
